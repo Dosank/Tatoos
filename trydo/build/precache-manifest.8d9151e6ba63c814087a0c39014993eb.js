33b02be5d87156ff949566eb2b03abade19b7f70
Dosank <juanfardin@gmail.com>
2020-07-12 20:25:10 -0300

    funciona?

---
+++ trydo/build/precache-manifest.8d9151e6ba63c814087a0c39014993eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8112f8228d5a7ea782647ba3ab01d7",
+    "url": "/tatoos/index.html"
+  },
+  {
+    "revision": "d61558caa221464c6955",
+    "url": "/tatoos/static/css/2.067c5a5e.chunk.css"
+  },
+  {
+    "revision": "e50ece45a5598ac90ce9",
+    "url": "/tatoos/static/css/main.7c421fc9.chunk.css"
+  },
+  {
+    "revision": "d61558caa221464c6955",
+    "url": "/tatoos/static/js/2.1d17a076.chunk.js"
+  },
+  {
+    "revision": "e50ece45a5598ac90ce9",
+    "url": "/tatoos/static/js/main.a4144dff.chunk.js"
+  },
+  {
+    "revision": "26d39b350d835c72be35",
+    "url": "/tatoos/static/js/runtime~main.b3943214.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tatoos/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tatoos/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tatoos/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tatoos/static/media/slick.f97e3bbf.svg"
+  }
+]);
